76ff3ea1f491670f81e80631926254ac632d5f79
VincentYang <1799327707@qq.com>
2025-03-17 21:08:52 +0800

    [ 优化 ] 文件夹位置优化

--- testDemos/myPredict.py
+++ testDemos/myPredict.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # 加载预训练模型
-model = YOLO('../best.pt')
+model = YOLO('../runs/detect/train/weights/best.pt')
 
 # 执行预测
 results = model.predict(
-    source='../imageSets',  # 输入源路径
-    show=True,         # 实时显示检测结果
+    source='D://testData',  # 输入源路径
+    show=False,         # 实时显示检测结果
     save=True,         # 自动保存带标注的图片
     conf=0.25,         # 置信度阈值(默认值)
     save_txt=False,    # 是否保存检测结果为txt文件
