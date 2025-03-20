73602b3d7fb7f454f4eb7d5de0e1d3170bafcf1e
VincentYang <1799327707@qq.com>
2025-03-20 19:32:12 +0800

    服务器代码配置完成

--- testDemos/myPredict.py
+++ testDemos/myPredict.py
@@ -1,15 +1,37 @@
 from ultralytics import YOLO
 
 # 加载预训练模型
-model = YOLO('../runs/detect/train/weights/best.pt')
+model = YOLO('../models/yolov8m.pt')
 
 # 执行预测
 results = model.predict(
-    source='D://testData',  # 输入源路径
-    show=False,         # 实时显示检测结果
-    save=True,         # 自动保存带标注的图片
-    conf=0.25,         # 置信度阈值(默认值)
-    save_txt=False,    # 是否保存检测结果为txt文件
-    save_conf=False    # 在保存的txt中是否包含置信度
+    source='/data/AIfusion/liuzhongyun/Datasets/BDD100K/bdd100k_images/bdd100k/images/100k/test/cabc30fc-e7726578.jpg',
+    project="runs",  # 自定义根目录
+    show=False,
+    save=True,
+    conf=0.25,
+    save_txt=False,
+    save_conf=False,
+    line_width=1  # 设置为1-3的整数，数值越小线条越细
 )
 
+# from ultralytics import YOLO
+#
+# model = YOLO('../runs/detect/train/weights/best.pt')
+# results = model.predict(
+#     source='D://testData',
+#     stream=True,  # <-- Mandatory for large datasets
+#     line_width=1,  # 设置为1-3的整数，数值越小线条越细
+#     save=True,
+#     conf=0.25,
+#     save_txt=False,
+#     save_conf=False,
+#     show=False,
+# )
+#
+# # 启用流式推理模式 节约内存占用
+# # 添加stream=True参数，将推理结果转换为生成器（generator），逐帧释放内存
+# for result in results:  # 逐帧处理，内存占用降低60-80%
+#     boxes = result.boxes  # 实时处理并释放内存
+#     masks = result.masks  # Segmentation masks (also affected)
+#     probs = result.probs  # Classification outputs
